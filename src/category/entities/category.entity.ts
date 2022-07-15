737b64907fdbfab37a5ec3765eb861267808573e
WagnerRR25 <103352223+WagnerRR25@users.noreply.github.com>
2022-07-15 05:57:20 -0300

    category

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,13 @@
-export class Category {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'Category' })
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  label: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+}
